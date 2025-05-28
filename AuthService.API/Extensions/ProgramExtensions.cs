bc8175077a859075bdcd56a2d7881ddb493fc290
nczescik <nczescik3@gmail.com>
2025-05-28 11:53:48 +0200

    Added extansions for Program.cs and swagger autentication

---
+++ AuthService.API/Extensions/ProgramExtensions.cs
@@ -0,0 +1,95 @@
+﻿using AuthService.API.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using RabbitMQ.Client;
+using Shared.Infrastructure.Messaging.RabbitMQ;
+using System.Text;
+
+namespace AuthService.API.Extensions
+{
+    public static class ProgramExtensions
+    {
+
+        public static IServiceCollection AddSwaggerExt(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+            return services;
+        }
+
+        public static IServiceCollection AddAuthenticationExt(this IServiceCollection services, string key)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+        {
+            services
+                .AddScoped<IJwtTokenGenerator, JwtTokenGenerator>()
+                .AddScoped<IEventPublisher, RabbitMqEventPublisher>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddRabbitMq(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            services.AddSingleton<IConnectionFactory>(sp => new ConnectionFactory
+            {
+                HostName = builder.Configuration["RabbitMQ:Host"] ?? "localhost"
+            });
+            return services;
+        }
+        public static IApplicationBuilder UseSwaggerExt(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+                options.RoutePrefix = string.Empty;
+            });
+            return app;
+        }
+    }
+}
--- AuthService.API/Program.cs
+++ AuthService.API/Program.cs
@@ -1,53 +1,27 @@
-﻿using AuthService.API.Security;
+﻿using AuthService.API.Extensions;
 using AuthService.Settings;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using RabbitMQ.Client;
-using Shared.Infrastructure.Messaging.RabbitMQ;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
-builder.Services.AddScoped<IEventPublisher, RabbitMqEventPublisher>();
-builder.Services.AddControllers();
-
 var jwtSection = builder.Configuration.GetSection("JwtSettings");
-builder.Services.Configure<JwtSettings>(jwtSection);
 
-var jwtKey = Encoding.ASCII.GetBytes(jwtSection["SecretKey"]!);
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerExt()
+    .AddDependencyInjection()
+    .AddRabbitMq(builder)
+    .Configure<JwtSettings>(jwtSection)
+    .AddAuthenticationExt(jwtSection.Key)
+    .AddControllers();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(opt =>
-    {
-        opt.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(jwtKey),
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidIssuer = jwtSection["Issuer"],
-            ValidAudience = jwtSection["Audience"]
-        };
-    });
-
-builder.Services.AddSingleton<IConnectionFactory>(sp => new ConnectionFactory
-{
-    HostName = builder.Configuration["RabbitMQ:Host"] ?? "localhost"
-});
 var app = builder.Build();
 
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseAuthentication()
+    .UseAuthorization();
 
-app.UseSwagger();
-app.UseSwaggerUI(options =>
+if (app.Environment.IsDevelopment())
 {
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
-    options.RoutePrefix = string.Empty;
-});
+    app.UseSwaggerExt();
+}
 
 app.MapControllers();
 
